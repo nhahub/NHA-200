81f23737dda56a9f4e751a2830415acbb0852e97
Ahmed Saeed <AhmedSaeed15298@gmail.com>
2025-11-11 13:55:06 +0200

    Project Structure v1.1

---
+++ SauceDemoWebsite/src/test/java/SauceDemoPagesTest/YourInformationPageTest.java
@@ -0,0 +1,39 @@
+package SauceDemoPagesTest;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class YourInformationPageTest {
+
+
+    @Test
+    public void validCheckout_ValidData()
+    {
+
+    }
+    @Test
+    public void inValidCheckout_LastNameFieldEmpty()
+    {
+
+    }
+
+    @Test
+    public void checkoutCancellation()
+    {
+
+    }
+
+
+    @BeforeMethod
+    public void testSessionStartup()
+    {
+
+    }
+
+    @AfterMethod
+    public void testSessionTearDown()
+    {
+
+    }
+}
