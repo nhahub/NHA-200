aa21eeed4393e0e939d07a33c4d707d372f8560f
MayarMagdy24 <mayar.elshafie@gmail.com>
2025-11-11 17:57:32 +0200

    CompletePageTest added OverviewPageTest added OverviewPage added ProductPage added ProductPageTest added

--- SauceDemoWebsite/src/test/java/SauceDemoPagesTest/CompletePageTest.java
+++ SauceDemoWebsite/src/test/java/SauceDemoPagesTest/CompletePageTest.java
@@ -5,8 +5,26 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CompletePageTest {
+   @Test
+   public void successfulCompleteionofproduct(){
+
+    }
     @Test
+    public void unsuccesfulcompleteionofproduct(){
+
+    }
 
 
 
+    @BeforeMethod
+    public void testSessionStart()
+    {
+
+    }
+
+    @AfterMethod
+    public void testSessionTearDown()
+    {
+
+    }
 }
--- SauceDemoWebsite/src/test/java/SauceDemoPagesTest/OverviewPageTest.java
+++ SauceDemoWebsite/src/test/java/SauceDemoPagesTest/OverviewPageTest.java
@@ -1,4 +1,38 @@
 package SauceDemoPagesTest;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class OverviewPageTest {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @BeforeMethod
+    public void testSessionStart()
+    {
+
+    }
+
+    @AfterMethod
+    public void testSessionTearDown()
+    {
+
+    }
 }
