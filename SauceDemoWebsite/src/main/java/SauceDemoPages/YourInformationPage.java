536acdeaafc9b5a1b1f54f58df7cdd4265a5bafa
MayarMagdy24 <mayar.elshafie@gmail.com>
2025-11-22 17:24:19 +0200

    CompletePageTest added OverviewPageTest added OverviewPage added ProductPage added ProductPageTest added

--- SauceDemoWebsite/src/main/java/SauceDemoPages/YourInformationPage.java
+++ SauceDemoWebsite/src/main/java/SauceDemoPages/YourInformationPage.java
@@ -1,30 +1,64 @@
 package SauceDemoPages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import java.net.URL;
+
 public class YourInformationPage {
+public WebDriver driver;
 
-    public void navigateToCheckoutPage()
+//Locators:
+private By firstNameField = By.id("first-name");
+    private By lastNameField = By.id("last-name");
+    private By postalCodeField = By.id("postal-code");
+    private By continueButton = By.id("continue");
+    private By cancelButton = By.id("cancel");
+    // Constructor
+    public YourInformationPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+
+
+
+
+    public void navigateToCheckoutPage(String URL)
     {
+        driver.get(URL);
 
     }
-    public void enterFirstName()
+    public void enterFirstName(String firstName)
     {
+        driver.findElement(firstNameField).sendKeys(firstName);
 
     }
-    public void enterLastName()
+    public void enterLastName(String lastName)
     {
-
+        driver.findElement(lastNameField).sendKeys(lastName);
     }
-    public void enterPostalCode()
+    public void enterPostalCode(String postalCode)
     {
+        driver.findElement(postalCodeField).sendKeys(postalCode);
 
     }
     public void clickOnContinueButton()
     {
+        driver.findElement(continueButton).click();
 
     }
     public void clickOnCancelButton()
     {
+        driver.findElement(cancelButton).click();
 
     }
-
+// ALL THE INFO
+// public void fillInformationAndContinue(String firstName, String lastName, String postalCode) {
+//        enterFirstName(firstName);
+//        enterLastName(lastName);
+//        enterPostalCode(postalCode);
+//        clickOnContinueButton();
+//    }
 }
--- SauceDemoWebsite/src/test/java/SauceDemoPagesTest/YourInformationPageTest.java
+++ SauceDemoWebsite/src/test/java/SauceDemoPagesTest/YourInformationPageTest.java
@@ -1,39 +1,58 @@
 package SauceDemoPagesTest;
 
+import SauceDemoPages.YourInformationPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class YourInformationPageTest {
 
-
+    private WebDriver driver;
+    private YourInformationPage infoPage;
     @Test
     public void validCheckout_ValidData()
     {
-
+infoPage.enterFirstName("M");
+infoPage.enterLastName("M");
+infoPage.enterPostalCode("12222");
+infoPage.clickOnContinueButton();
     }
     @Test
     public void inValidCheckout_LastNameFieldEmpty()
     {
 
+        infoPage.enterFirstName("M");
+        infoPage.enterLastName("");
+        infoPage.enterPostalCode("12222");
+        infoPage.clickOnContinueButton();
+
+        By errorMessage = By.cssSelector("h3[data-test='error']");
+        String errorText = driver.findElement(errorMessage).getText();
+        Assert.assertEquals(errorText, "Error: Last Name is required");
     }
 
     @Test
     public void checkoutCancellation()
     {
-
+        infoPage.clickOnCancelButton();
+        Assert.assertTrue(driver.getCurrentUrl().contains("cart.html"));
     }
 
 
     @BeforeMethod
     public void testSessionStartup()
     {
-
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @AfterMethod
     public void testSessionTearDown()
     {
-
+        driver.quit();
     }
 }
