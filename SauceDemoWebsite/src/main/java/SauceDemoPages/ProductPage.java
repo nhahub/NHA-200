2eb1fa543a831422900063bcf1bf5724a2157353
MayarMagdy24 <mayar.elshafie@gmail.com>
2025-11-22 17:36:05 +0200

    CompletePageTest added OverviewPageTest added OverviewPage added ProductPage added ProductPageTest added

--- SauceDemoWebsite/src/main/java/SauceDemoPages/ProductPage.java
+++ SauceDemoWebsite/src/main/java/SauceDemoPages/ProductPage.java
@@ -1,16 +1,41 @@
 package SauceDemoPages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class ProductPage {
-    public void navigateToProductpage(){
+
+    private WebDriver driver;
+    //Locators:
+    private By firstProductAddToCartButton = By.id("add-to-cart-sauce-labs-backpack");
+    private By firstProductRemoveButton = By.id("remove-sauce-labs-backpack");
+    private By backToProductsButton = By.id("back-to-products");
+
+
+    public ProductPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+
+
+
+
+    public void navigateToProductpage(String URL){
+
+        driver.get(URL);
 
     }
     public void clickOnAddToCartButton(){
+        driver.findElement(firstProductAddToCartButton).click();
 
     }
     public void clickOnRemoveButton(){
+        driver.findElement(firstProductRemoveButton).click();
 
     }
     public void clickOnBackToProductButton(){
+        driver.findElement(backToProductsButton).click();
 
     }
 
--- SauceDemoWebsite/src/test/java/SauceDemoPagesTest/ProductPageTest.java
+++ SauceDemoWebsite/src/test/java/SauceDemoPagesTest/ProductPageTest.java
@@ -1,39 +1,56 @@
 package SauceDemoPagesTest;
 
+import SauceDemoPages.ProductPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ProductPageTest
 {
+    private WebDriver driver;
+    private ProductPage productPage;
+
+
+
+    @BeforeMethod
+    public void testSessionStart()
+    {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+
 
     @Test
     public void addToCartButtonFunctionality()
     {
-
+        productPage.clickOnAddToCartButton();
+        Assert.assertTrue(driver.getPageSource().contains("REMOVE"), "Add to Cart failed");
     }
 
     @Test
     public void removeButtonFunctionality()
     {
-
-    }
+        productPage.clickOnAddToCartButton();
+        productPage.clickOnRemoveButton();
+        Assert.assertTrue(driver.getPageSource().contains("ADD TO CART"), "Remove button failed");}
 
     @Test
     public void backToProductButtonFunctionality()
     {
+        productPage.clickOnBackToProductButton();
 
     }
 
-    @BeforeMethod
-    public void testSessionStart()
-    {
 
-    }
 
     @AfterMethod
     public void testSessionTearDown()
     {
+        driver.quit();
 
     }
 
