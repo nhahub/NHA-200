f1dfa246de8bd0f797239539555c94361074de33
MayarMagdy24 <mayar.elshafie@gmail.com>
2025-11-22 16:41:56 +0200

    CompletePageTest added OverviewPageTest added OverviewPage added ProductPage added ProductPageTest added

--- SauceDemoWebsite/src/main/java/SauceDemoPages/CompletePage.java
+++ SauceDemoWebsite/src/main/java/SauceDemoPages/CompletePage.java
@@ -2,6 +2,10 @@ package SauceDemoPages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CompletePage {
 
@@ -12,6 +16,7 @@ public class CompletePage {
     public By successMessage = By.className("complete-header");
 
     public CompletePage(WebDriver driver) {
+        this.driver = driver;
     }
 
 
@@ -26,7 +31,8 @@ public class CompletePage {
         driver.findElement(backHomeButton).click();
     }
     public String getSuccessMessage() {
-        return driver.findElement(successMessage).getText();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(successMessage)); return driver.findElement(successMessage).getText();
     }
 }
 
--- SauceDemoWebsite/src/test/java/SauceDemoPagesTest/CompletePageTest.java
+++ SauceDemoWebsite/src/test/java/SauceDemoPagesTest/CompletePageTest.java
@@ -2,6 +2,7 @@ package SauceDemoPagesTest;
 
 import SauceDemoPages.CompletePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -10,21 +11,26 @@ import org.testng.annotations.Test;
 public class CompletePageTest {
     public CompletePage completePage;
     public WebDriver driver;
+    @BeforeMethod
+    public void testSessionStart()
+    {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        completePage = new CompletePage(driver);
+        completePage.navigateToCompletePage("https://www.saucedemo.com/checkout-complete.html");
+    }
+
+
     @Test
    public void backHomeButtonFunctionality()
    {
        String message = completePage.getSuccessMessage();
        Assert.assertEquals(message, "Thank you for your order!");
+       completePage.clickOnBackHomeButton();
+
    }
 
 
-    @BeforeMethod
-    public void testSessionStart()
-    {
-
-        completePage = new CompletePage(driver);
-        completePage.navigateToCompletePage("https://www.saucedemo.com/checkout-complete.html");
-    }
 
     @AfterMethod
     public void testSessionTearDown()
