d3a9130c5fa70e33bec8ec1f6f0d0439e40a721f
MayarMagdy24 <mayar.elshafie@gmail.com>
2025-11-22 16:29:26 +0200

    CompletePageTest added OverviewPageTest added OverviewPage added ProductPage added ProductPageTest added

--- SauceDemoWebsite/src/main/java/SauceDemoPages/CompletePage.java
+++ SauceDemoWebsite/src/main/java/SauceDemoPages/CompletePage.java
@@ -1,18 +1,33 @@
 package SauceDemoPages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class CompletePage {
 
+    public WebDriver driver;
+    //Locators :
+    public By finishButton = By.id("finish");
+    public By backHomeButton = By.id("back-to-products");
+    public By successMessage = By.className("complete-header");
 
-    public void navigateToCompletePage(){
+    public CompletePage(WebDriver driver) {
+    }
 
+
+    public void navigateToCompletePage(String URL){
+
+        driver.get(URL);
     }
     public void clickOnFinishButton(){
-
+        driver.findElement(finishButton).click();
     }
     public void clickOnBackHomeButton(){
-
+        driver.findElement(backHomeButton).click();
     }
-
+    public String getSuccessMessage() {
+        return driver.findElement(successMessage).getText();
+    }
 }
 
 
--- SauceDemoWebsite/src/test/java/SauceDemoPagesTest/CompletePageTest.java
+++ SauceDemoWebsite/src/test/java/SauceDemoPagesTest/CompletePageTest.java
@@ -1,14 +1,20 @@
 package SauceDemoPagesTest;
 
+import SauceDemoPages.CompletePage;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CompletePageTest {
-   @Test
+    public CompletePage completePage;
+    public WebDriver driver;
+    @Test
    public void backHomeButtonFunctionality()
    {
-
+       String message = completePage.getSuccessMessage();
+       Assert.assertEquals(message, "Thank you for your order!");
    }
 
 
@@ -16,11 +22,13 @@ public class CompletePageTest {
     public void testSessionStart()
     {
 
+        completePage = new CompletePage(driver);
+        completePage.navigateToCompletePage("https://www.saucedemo.com/checkout-complete.html");
     }
 
     @AfterMethod
     public void testSessionTearDown()
     {
-
+driver.quit();
     }
 }
